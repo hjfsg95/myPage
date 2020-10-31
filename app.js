849071cc5e67729fb2d626e39ddd9f2f205bf64b
hjfsg95 <littlecoolfish@163.com>
2020-10-31 18:49:04 -0500

    mypage - first draft

---
+++ app.js
@@ -0,0 +1,8 @@
+var wine = document.querySelector("#wine")
+
+wine.addEventListener("click", (e) => {
+//   e.preventDefault();
+  window.location.href = "projects/wine.html";
+});
+
+// 
---
+++ projects/skin_cancer/style.js
@@ -0,0 +1,4 @@
+
+$(".content h3").addClass("paint");
+$(".content h4").addClass("paint-lower");
+$(".content b").addClass("subsubtitle");
